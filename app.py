545a0593a31f2ad53dd68fca937280e81684152c
Marlon Ramirez <marlonramr99@gmail.com>
2026-05-08 15:52:11 -0500

    Actualización de base de datos a DTAVO

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-key-bendito')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@localhost/benditoencanto')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@localhost/DTAVO')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
